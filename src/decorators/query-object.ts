2ca834ba8c2085a924e92ab22efc9a1996e7a7f8
budiadiono <budiadiono@gmail.com>
2019-07-16 15:53:26 +0700

    added query object resolver

--- src/decorators/query-object.ts
+++ src/decorators/query-object.ts
@@ -1,10 +1,10 @@
 import { QueryObjectMeta } from '../lib/query-object-meta'
 import { getMeta } from '../lib/utils'
 
-export function queryObject(): ParameterDecorator {
+export function queryObject<T>(resolver?: (obj: any) => T): ParameterDecorator {
   return (target: object, key: string, index: number) => {
     getMeta(target).queryObjectParameters.push(
-      new QueryObjectMeta(key, index)
+      new QueryObjectMeta(key, index, resolver)
     )
   }
 }
--- src/lib/query-object-meta.ts
+++ src/lib/query-object-meta.ts
@@ -1,7 +1,7 @@
 import { RouterContext } from 'koa-router'
 import { IParamMeta } from '../types'
 
-export class QueryObjectMeta implements IParamMeta {
+export class QueryObjectMeta<T = any> implements IParamMeta {
   /**
    * name of method
    */
@@ -12,12 +12,21 @@ export class QueryObjectMeta implements IParamMeta {
    */
   index: number
 
-  constructor(key: string, index: number) {
+  /**
+   * resolver callback
+   */
+  resolver?: (obj: any) => T
+
+  constructor(key: string, index: number, resolver?: (obj: any) => T) {
     this.method = key
     this.index = index
+    this.resolver = resolver
   }
 
   getValue(context: RouterContext) {
+    if (this.resolver) {
+      return this.resolver(context.query)
+    }
     return context.query
   }
 }
