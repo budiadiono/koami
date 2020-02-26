a1e971a3c906391edf25dd62ecc246eaf941bfef
budiadiono <budiadiono@gmail.com>
2020-02-26 22:02:03 +0700

    fix validationErrors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import Boom from '@hapi/boom'
-import { ValidationError } from 'yup'
+import * as yup from 'yup'
 import { ValidateType } from '../types'
 import { ControllerMeta } from './controller-meta'
 
@@ -13,14 +13,28 @@ export function getMeta(object: any): ControllerMeta {
 export function validateValue(value: any, validate?: ValidateType) {
   if (validate) {
     if (typeof validate === 'function') {
-      return validate(value)
-    } else {
-      try {
-        return validate.validateSync(value)
-      } catch (error) {
-        if (error instanceof ValidationError) {
-          throw Boom.badRequest(error.message)
-        }
+      validate = yup.object(validate(yup))
+    }
+
+    try {
+      return validate.validateSync(value, {
+        abortEarly: false
+      })
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        const e = Boom.badRequest(error.message)
+
+        e.output.statusCode = 400
+
+        // tslint:disable-next-line: no-string-literal
+        e.output.payload['validationErrors'] = error.inner.map(err => ({
+          path: err.path,
+          message: err.errors.join(', ')
+        }))
+
+        e.reformat()
+
+        throw e
       }
     }
   }
